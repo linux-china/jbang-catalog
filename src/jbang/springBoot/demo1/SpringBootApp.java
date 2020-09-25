a3e0df7efa27e7259520e5d78fa4b80a5265e5b5
linux_china <libing.chen@gmail.com>
2020-09-24 23:48:55 -0700

    add nick from @Value

--- src/jbang/springBoot/demo1/SpringBootApp.java
+++ src/jbang/springBoot/demo1/SpringBootApp.java
@@ -1,13 +1,17 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS org.springframework.boot:spring-boot-starter:2.3.4.RELEASE
+//FILES application.properties
 package demo1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootApp implements CommandLineRunner {
+    @Value("${nick}")
+    private String nick;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
@@ -15,6 +19,6 @@ public class SpringBootApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("App started");
+        System.out.println("Hello " + nick);
     }
 }
