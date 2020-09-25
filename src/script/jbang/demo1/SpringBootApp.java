ec8a4f75f25e169bda44734f4960662d18fcb1d3
linux_china <libing.chen@gmail.com>
2020-09-24 17:59:13 -0700

    SpringBootApp

---
+++ src/script/jbang/demo1/SpringBootApp.java
@@ -0,0 +1,20 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS org.springframework.boot:spring-boot-starter:2.3.4.RELEASE
+package demo1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("started");
+    }
+}
