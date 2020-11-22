f39938c72d94c67ff177f7ffdc03a1393d100801
linux_china <libing.chen@gmail.com>
2020-11-21 17:54:51 -0800

    add Dotenv

--- src/jbang/hello/HelloWorld.java
+++ src/jbang/hello/HelloWorld.java
@@ -1,8 +1,10 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS org.slf4j:slf4j-simple:1.7.30
-//DEPS org.projectlombok:lombok:1.18.12
+//DEPS org.projectlombok:lombok:1.18.16
+//DEPS io.github.cdimascio:dotenv-java:2.2.0
 //SOURCES UserManager.java
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,6 +15,8 @@ public class HelloWorld {
         System.out.println("Hello world!");
         System.out.println("Java version: " + System.getProperty("java.version"));
         System.out.println(new UserManager().findNickById(1));
+        String nick = Dotenv.load().get("nick");
+        System.out.println(nick);
     }
 
     @Data
