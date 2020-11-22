0df0a19cec1736f846528827fc034db2f6b920f2
linux_china <libing.chen@gmail.com>
2020-11-21 18:20:09 -0800

    polishing

--- src/jbang/springBoot/demo1/SpringBootApp.java
+++ src/jbang/springBoot/demo1/SpringBootApp.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS org.springframework.boot:spring-boot-starter:2.3.4.RELEASE
+//DEPS org.springframework.boot:spring-boot-starter:2.4.0
 //FILES application.properties
 package demo1;
 
