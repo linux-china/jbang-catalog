0ef9d4ca253a7a4300e8a56c42b72af798c3ce05
linux_china <libing.chen@gmail.com>
2021-01-17 14:46:28 -0800

    Add Picocli

--- src/main/java/org/mvnsearch/App.java
+++ src/main/java/org/mvnsearch/App.java
@@ -1,8 +1,29 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS info.picocli:picocli:4.5.2
+//DEPS info.picocli:picocli-codegen:4.5.2
 package org.mvnsearch;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "Hello", mixinStandardHelpOptions = true, version = "Hello 0.1",
+        description = "Hello made with jbang")
+public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
+    private String greeting;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        System.out.println("Hello " + greeting);
+        return 0;
     }
 }
