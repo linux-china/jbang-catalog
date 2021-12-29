9309a0379417ab8db00a5fe7d2f5883687287b88
linux_china <libing.chen@gmail.com>
2021-12-28 23:48:43 -0800

    Update dependencies

--- src/hello/java/HelloWorld.java
+++ src/hello/java/HelloWorld.java
@@ -2,11 +2,11 @@
 //JAVA 17
 //DEPS org.slf4j:slf4j-simple:1.7.32
 //DEPS org.projectlombok:lombok:1.18.22
-//DEPS io.github.cdimascio:dotenv-java:2.2.0
+//DEPS io.github.cdimascio:dotenv-java:2.2.3
 
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j;  
 
 @Slf4j
 public class HelloWorld {
